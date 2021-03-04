7d0c0c2e582ae395ec1417eb07be64c06450efc1
cnsgfirf <69499660+cnsgfirf@users.noreply.github.com>
2021-03-04 06:37:19 +0200

    need

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def draw_hose(x, y, width, height):
+    """
+    function_draw house
+    :param x: courdinator house
+    :param y: couse height
+    :param width:
+    :param height:
+    :return:
+    """
     print(' i draw house ....', x, y, width, height)
 
 

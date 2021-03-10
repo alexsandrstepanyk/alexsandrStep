5e8d672e2c4d355d474ee4f6a7ecab7e94c805d7
cnsgfirf <69499660+cnsgfirf@users.noreply.github.com>
2021-03-10 09:21:14 +0200

    object git get gommit

--- list.py
+++ list.py
@@ -1,4 +1,6 @@
-int_list = [1,2,3]
+from typing import List
+
+int_list = [1, 2, 3]
 print(int_list)
 len(int_list)
 print(int_list[0])
@@ -9,21 +11,34 @@ names2 = ['racy', 'joly', 'iren']
 print(names2 + names1)
 names1[0] = 'liam'
 print(names1)
-names1. append('wolia')# plus new element for this list
+names1.append('wolia')  # plus new element for this list
 names2.append('mila')
 print(names2 + names1)
-popped = names1.pop()# del  first element for this list
+popped = names1.pop()  # del  first element for this list
 print(popped)
 print(names1)
-names1. pop(1)# del index element for this list
+names1.pop(1)  # del index element for this list
 print(names1)
 names1.sort()
 print(names1)
 names1.append('igor')
 print(names1)
-letter = ['ac', 'ab', 'aa']#sort this list
+letter = ['ac', 'ab', 'aa']  # sort this list
 letter.sort()
 print(letter)
 letters = ['abcd', 'abc', 'ab', 'a']
 letters.sort(key=len)
 print(letters)
+numbers = [3, 2, 8, 9, 0, 5, 4, 3, 2, 1]
+numbers.sort()
+print(numbers)
+yers: list[int] = [2000, 1900, 2001, 1994, 1993, 2021]
+yers.sort()
+print(yers)
+yers.reverse()  # sort revers
+print(yers)
+yers.sort(reverse=True)  # sort down
+print(yers)
+yers.insert(3, 1988)  # add new certain element
+print(yers)
+print(yers.count(2000))

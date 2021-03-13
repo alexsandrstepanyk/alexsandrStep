b9625d13355b48db776e1750d8f1203512699168
cnsgfirf <69499660+cnsgfirf@users.noreply.github.com>
2021-03-13 06:09:24 +0200

    object git get gommit

--- order_dict.py
+++ order_dict.py
@@ -13,14 +13,39 @@ d3['a'] = 'B'
 d3['b'] = 'A'
 d3['c'] = 'C'
 
-
 d4 = {}
 d4['a'] = 'B'
 d4['b'] = 'A'
 d4['c'] = 'C'
-print(d1==d2)
-print(d1==d3)
-print(d2==d4)
+print(d1 == d2)
+print(d1 == d3)
+print(d2 == d4)
 
 for k, v in d1.items():
     print(k, v)
+
+from collections import OrderedDict
+d1 = OrderedDict()
+d1['a'] = 'A'
+d1['b'] = 'B'
+d1['c'] = 'C'
+
+d2 = OrderedDict()
+d2['a'] = 'B'
+d2['b'] = 'A'
+d2['c'] = 'C'
+
+d3 = OrderedDict()
+d3['a'] = 'B'
+d3['b'] = 'A'
+d3['c'] = 'C'
+
+d4 = OrderedDict()
+d4['a'] = 'B'
+d4['b'] = 'A'
+d4['c'] = 'C'
+
+print(d1 == d2)
+print(d1 == d3)
+print(d2 == d4)
+print(d2 == d3)
---
+++ tuple.py
@@ -0,0 +1,10 @@
+person = ('Andrei', 'Konchenko', '28',
+          'Andrei', 'Konchenko', '23') #tuple
+print(person[0])
+person_1 = ['Andrei', 'Konchenko', '28'] #list
+
+person_1[1] = 'vaschuk' #list
+print(person_1) #list
+
+print(person.count('Konchenko'))# input element
+print(person.index('23'))
